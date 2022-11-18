db8bd7286667acf56f2d6ed5f580e7d2a3ff696d
diana spieser <dianaspieser@gmail.com>
2022-11-18 10:14:56 +0100

    ok

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "Impossible.The movies has already been added to the list" }
+end
