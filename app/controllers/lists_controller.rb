e2bd279b58fb2bd16f4af7a923784c8b990574cc
diana spieser <dianaspieser@gmail.com>
2022-11-18 15:38:21 +0100

    ok

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,7 +12,7 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.create(list_params)
+    @list = List.new(list_params)
     if @list.save
       redirect_to list_path(@list)
     else
@@ -24,7 +24,7 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 
   def set_list
